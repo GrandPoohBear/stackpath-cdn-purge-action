8d7df7742e51660614f04fe496daca401cc65015
Andy Isaacson <andy@drivetime.fm>
2020-09-21 18:46:58 -0700

    Remove logging

--- index.js
+++ index.js
@@ -11,16 +11,10 @@ const OAUTH_ENDPOINT = "https://gateway.stackpath.com/identity/v1/oauth2/token";
     const stackId = core.getInput("stackId");
     const url = core.getInput("url");
 
-    console.log({
-      clientId,
-      stackId,
-      url,
-    });
-
     const response = await fetch(OAUTH_ENDPOINT, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "content-type": "application/json",
       },
       body: JSON.stringify({
         client_id: clientId,
@@ -31,13 +25,13 @@ const OAUTH_ENDPOINT = "https://gateway.stackpath.com/identity/v1/oauth2/token";
     const json = await response.json();
 
     const accessToken = json.access_token;
-    console.log(`Access token: ${accessToken}`);
     const purgeResponse = await fetch(
       `https://gateway.stackpath.com/cdn/v1/stacks/${stackId}/purge`,
       {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "content-type": "application/json",
+          accept: "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify({
@@ -45,6 +39,7 @@ const OAUTH_ENDPOINT = "https://gateway.stackpath.com/identity/v1/oauth2/token";
             {
               url: url,
               recursive: true,
+              invalidateOnly: true,
             },
           ],
         }),
